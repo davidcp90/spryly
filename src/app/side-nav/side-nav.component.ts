97f45284c5230db771142d2b65c2639baaa35b1d
David Camargo <davidcp90@gmail.com>
2016-06-22 17:51:00 -0500

    Upgraded versions of angular please perform npm i

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ROUTER_DIRECTIVES } from '@angular/router';
 @Component({
 	selector: 'spr-side-nav',
-	template: require ('./side-nav.component.html')
+	template: require ('./side-nav.component.html'),
+	directives: [ROUTER_DIRECTIVES]
 })
 export class SideNavComponent implements OnInit {
 	constructor() {}
