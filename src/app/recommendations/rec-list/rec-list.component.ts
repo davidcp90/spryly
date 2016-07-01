45bd5cd2c1b49c7d7f82d3ab28732f5c458ff549
David Camargo <davidcp90@gmail.com>
2016-07-01 14:54:51 -0500

    card recommendations

--- src/app/recommendations/rec-list/rec-list.component.ts
+++ src/app/recommendations/rec-list/rec-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RecDetailComponent } from '../rec-detail';
 @Component({
   selector: 'spr-rec-list',
-  template: require('./rec-list.component.html')
+  template: require('./rec-list.component.html'),
+  directives: [RecDetailComponent]
 })
 
 export class RecListComponent implements OnInit {
