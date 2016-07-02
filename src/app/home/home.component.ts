3f7d05ccd93323c3609cae47a88cc6578d79f2e6
David Camargo <davidcp90@gmail.com>
2016-07-01 23:57:24 -0500

    home al 85%

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { PublicNavComponent } from '../public-nav';
 @Component({
   selector: 'spr-home',
-  template: require('./home.component.html')
+  template: require('./home.component.html'),
+  directives: [PublicNavComponent],
+  styles: [ require('./home.style.sass') ]
 })
 export class HomeComponent implements OnInit {
-  constructor() {
-    // Do stuff
+
+  public constructor() {
   }
 
   ngOnInit() {
---
+++ src/app/public-nav/public-nav.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+@Component({
+  selector: 'spr-public-nav',
+  template: require ('./public-nav.component.html'),
+  styles: [ require ('./public-nav.style.sass')],
+  directives: [ROUTER_DIRECTIVES]
+})
+export class PublicNavComponent implements OnInit {
+  constructor() {}
+  ngOnInit() {
+  }
+  toggleSidenav(){
+    let sidenav=document.getElementById('sidenav');
+    if(!sidenav.className.match('show')){
+      sidenav.className += " show";
+    }
+    else{
+      sidenav.className=sidenav.className.replace("show","");
+    }
+  }
+}
