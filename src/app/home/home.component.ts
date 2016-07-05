5666dbad53596f29b94a69e7317adfa03c7eb9ec
David Camargo <davidcp90@gmail.com>
2016-07-05 12:10:44 -0500

    home fixed on xs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { PublicNavComponent } from '../public-nav';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 @Component({
   selector: 'spr-home',
   template: require('./home.component.html'),
-  directives: [PublicNavComponent],
+  directives: [PublicNavComponent, ROUTER_DIRECTIVES],
   styles: [ require('./home.style.sass') ]
 })
 export class HomeComponent implements OnInit {
